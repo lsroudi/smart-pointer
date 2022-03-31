9c0a678082d3dafcb10a1539976b7cffe03b92f5
Abdessamad el akroudi <lsroudi@gmail.com>
2022-03-31 23:00:40 +0200

    test refcell

--- src/lib.rs
+++ src/lib.rs
@@ -103,4 +103,98 @@ pub fn rc_method(){
     let b = Cons(3, Box::new(a));
     //Demonstrating we’re not allowed to have two lists using Box<T> that try to share ownership of a third list
     //let c = Cons(4, Box::new(a));
+
 }
+
+pub fn rc_demo(){
+
+    enum List {
+        Cons(i32, Rc<List>),
+        Nil,
+    }
+    use List::{Cons, Nil};
+    use std::rc::Rc;
+    // Output
+    // count after creating a = 1
+    // count after creating b = 2
+    // count after creating c = 3
+    // count after c goes out of scope = 2
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+}
+
+//interior_mutability
+    pub trait Messenger {
+        fn send(&self, msg:&str);
+    }
+    pub struct LimitTracker<'a, T: 'a + Messenger> {
+        messenger: &'a T,
+        value: usize,
+        max: usize,
+    }
+    
+    impl<'a, T> LimitTracker<'a, T>
+        where T: Messenger {
+        pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+            LimitTracker {
+                messenger,
+                value: 0,
+                max,
+            }
+        }     
+        pub fn set_value(&mut self, value: usize) {
+            self.value = value;
+        
+            let percentage_of_max = self.value as f64 / self.max as f64;
+        
+            if percentage_of_max >= 1.0 {
+                self.messenger.send("Error: You are over your quota!");
+            } else if percentage_of_max >= 0.9 {
+                    self.messenger.send("Urgent warning: You've used up over 90% of
+                    your quota!");
+                } else if percentage_of_max >= 0.75 {
+                    self.messenger.send("Warning: You've used up over 75% of your
+                    quota!");
+                }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+    use std::cell::RefCell;
+
+    use super::*;
+
+    struct MockMessenger {
+    sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+    fn new() -> MockMessenger {
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
+        }
+    }
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+        fn it_sends_an_over_75_percent_warning_message() {
+            let mock_messenger = MockMessenger::new();
+            let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+            limit_tracker.set_value(80);
+
+            assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+            }
+    }
+
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ fn main() {
     smart_pointer::dereference_opertator();
     smart_pointer::my_box_type();
     smart_pointer::drop_method();
+    smart_pointer::rc_demo();
                                 
 }
